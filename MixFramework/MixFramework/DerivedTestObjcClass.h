c44a5e3501ca120d42b785132c930c56ba0a398f
Sambit <sambit.sarkar@hyland.com>
2019-04-12 20:58:54 +0530

    Updated

---
+++ MixFramework/MixFramework/DerivedTestObjcClass.h
@@ -0,0 +1,17 @@
+//
+//  DerivedTestObjcClass.h
+//  MixFramework
+//
+//  Created by Sambit Sarkar on 12/04/19.
+//  Copyright © 2019 Sambit Sarkar. All rights reserved.
+//
+
+#import <MixFramework/MixFramework.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface DerivedTestObjcClass : TestObjCClass
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ MixFramework/MixFramework/DerivedTestObjcClass.m
@@ -0,0 +1,13 @@
+//
+//  DerivedTestObjcClass.m
+//  MixFramework
+//
+//  Created by Sambit Sarkar on 12/04/19.
+//  Copyright © 2019 Sambit Sarkar. All rights reserved.
+//
+
+#import "DerivedTestObjcClass.h"
+
+@implementation DerivedTestObjcClass
+
+@end
